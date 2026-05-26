5af212d6110df614a2105d576f7890e72c5459ef
Madhavan Raja <madhavanraja99@gmail.com>
2026-05-25 19:53:12 -0700

    refactor: move autorun config inside server

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
+	"net"
+	"time"
 
 	"github.com/madhavan-raja/autorun/autorun"
 	"github.com/madhavan-raja/autorun/internal"
+	"github.com/madhavan-raja/autorun/internal/types"
+	"google.golang.org/grpc"
 )
 
 var logger *slog.Logger
@@ -13,9 +19,62 @@ func init() {
 	logger = internal.Logger
 }
 
-func main() {
-	a := autorun.New("")
-	a.LoadProcesses()
+type autorunServer struct {
+	autorun.UnimplementedAutorunServer
+	processes []types.Process
+}
 
-	a.Start()
+func (a *autorunServer) RunProcesses() {
+	for _, p := range a.processes {
+		go func() {
+			if p.RunOnStart {
+				logger.Info("Running Process On Start", "process", p.Name)
+			}
+
+			if p.Repeat {
+				ticker := time.NewTicker(time.Duration(p.Interval) * time.Second)
+				defer ticker.Stop()
+
+				for range ticker.C {
+					logger.Info("Running Process", "process", p.Name)
+				}
+			}
+		}()
+	}
+}
+
+func (a *autorunServer) Create(ctx context.Context, req *autorun.CreateRequest) (*autorun.CreateResponse, error) {
+	logger.Info("Received", "create_request", req)
+	return &autorun.CreateResponse{}, nil
+}
+
+
+func main() {
+	port := uint32(5678)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		logger.Error("Cannot create listener: %v", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	server := &autorunServer{
+		processes: []types.Process{
+			{
+				Name:        "Test",
+				Description: "Test Process",
+				Cmd:         "echo 'Hello World'",
+				RunOnStart:  false,
+				Repeat:      true,
+				Interval:    5,
+			},
+		},
+	}
+
+	server.RunProcesses()
+
+	autorun.RegisterAutorunServer(serverRegistrar, server)
+	if err = serverRegistrar.Serve(lis); err != nil {
+		logger.Error("Cannot serve: %s", err)
+	}
 }
